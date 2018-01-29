0850e11a98d0b5db4825ce8de9da485cdd5c43da
YuLaiZ <yulai_z@hotmail.com>
2018-01-30 00:19:57 +0800

    update d_user.sql

--- sql/d_user.sql
+++ sql/d_user.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50719
  File Encoding         : 65001
 
- Date: 30/01/2018 00:14:52
+ Date: 30/01/2018 00:18:54
 */
 
 SET NAMES utf8mb4;
@@ -29,13 +29,13 @@ CREATE TABLE `d_user`  (
   `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `role` enum('ADMIN','NORMAL') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'NORMAL',
   `register_token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `flag` tinyint(4) NULL DEFAULT 1,
+  `flag` tinyint(4) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `d_user_idx`(`user_name`, `password`) USING BTREE,
   INDEX `d_user_idx_uuid`(`uuid`) USING BTREE,
   INDEX `d_user_idx_role`(`role`) USING BTREE,
   INDEX `d_user_idx_token`(`register_token`) USING BTREE,
   INDEX `d_user_flag`(`flag`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
