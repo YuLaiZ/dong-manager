490c918934cbf6189880ba680a09e052cbc2df58
YuLaiZ <yulai_z@hotmail.com>
2018-01-31 00:44:53 +0800

    1.更新swagger-ui信息 2.更新细节文字显示 3.更新README

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
 # dong-manager
 董师傅的妖孽人生后台管理接口
+- 后台项目地址：[http://dong.yulaiz.com/dong-manager/](http://dong.yulaiz.com/dong-manager/)
+- 完整项目地址：[http://dong.yulaiz.com/](http://dong.yulaiz.com/)
+- 前端GitHub地址：[https://github.com/sosohime/fuckRPG](https://github.com/sosohime/fuckRPG)
+- 后台GitHub地址：[https://github.com/YuLaiZ/dong-manager](https://github.com/YuLaiZ/dong-manager)
 
-已部署的项目接口文档地址：[http://dong.yulaiz.com/dong-manager/](http://dong.yulaiz.com/dong-manager/)
-项目地址：[http://dong.yulaiz.com/](http://dong.yulaiz.com/)
-
-使用的框架与资源
+##### 使用的框架与资源
 1. spring boot
 2. mybatis
 3. mysql
 4. redis
 5. lombok
 6. swagger-ui
-
-前端GitHub地址：[https://github.com/sosohime/fuckRPG](https://github.com/sosohime/fuckRPG)
-
-后台GitHub地址：[https://github.com/YuLaiZ/dong-manager](https://github.com/YuLaiZ/dong-manager)
--- src/main/java/com/yulaiz/dong/web/Swagger2.java
+++ src/main/java/com/yulaiz/dong/web/Swagger2.java
@@ -7,6 +7,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -34,7 +35,7 @@ public class Swagger2 {
                 .title("dong-manager")
                 .description("董师傅的妖孽人生后台管理接口")
                 .termsOfServiceUrl("http://dong.yulaiz.com/")
-                .contact("YuLaiZ")
+                .contact(new Contact("YuLaiZ", "http://yulaiz.com/", "yulai_z@hotmail.com"))
                 .version("1.0")
                 .build();
     }
