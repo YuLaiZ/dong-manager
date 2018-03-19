8a26fe39b4695b659a99266a0400292016c15294
YuLaiZ <yulai_z@hotmail.com>
2018-03-19 17:54:45 +0800

    1.  修改 Swagger-UI 版本为2.7.0

--- src/main/java/com/yulaiz/dong/web/common/config/RedisConfiguration.java
+++ src/main/java/com/yulaiz/dong/web/common/config/RedisConfiguration.java
@@ -16,7 +16,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfiguration {
     /**
-     * redisTemplate 序列化使用的jdkSerializeable, 存储二进制字节码, 所以自定义序列化类
+     * redisTemplate 序列化使用的jdk Serializeable, 存储二进制字节码, 所以自定义序列化类
      *
      * @param redisConnectionFactory
      * @return
@@ -25,16 +25,12 @@ public class RedisConfiguration {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-
         // 使用Jackson2JsonRedisSerialize 替换默认序列化
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-
         jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
-
         // 设置value的序列化规则和 key的序列化规则
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
